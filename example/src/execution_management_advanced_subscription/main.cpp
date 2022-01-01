d2948671bcb6d262008a08aad99a17b8d79a624e
Crypto Chassis <admin@cryptochassis.com>
2021-12-31 16:36:43 -0800

    test

--- example/src/execution_management_advanced_subscription/main.cpp
+++ example/src/execution_management_advanced_subscription/main.cpp
@@ -1,6 +1,18 @@
 #include "ccapi_cpp/ccapi_session.h"
 namespace ccapi {
-Logger* Logger::logger = nullptr;  // This line is needed.
+class MyLogger final : public Logger {
+ public:
+  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
+                  const std::string& lineNumber, const std::string& message) override {
+    std::lock_guard<std::mutex> lock(m);
+    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
+  }
+
+ private:
+  std::mutex m;
+};
+MyLogger myLogger;
+Logger* Logger::logger = &myLogger;
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
@@ -8,11 +20,11 @@ class MyEventHandler : public EventHandler {
       std::cout << "Received an event of type SUBSCRIPTION_STATUS:\n" + event.toStringPretty(2, 2) << std::endl;
       auto message = event.getMessageList().at(0);
       if (message.getType() == Message::Type::SUBSCRIPTION_STARTED) {
-        Request request(Request::Operation::CREATE_ORDER, "okex", "BTC-USDT", "same correlation id for subscription and request");
+        Request request(Request::Operation::CANCEL_ORDER, "cryptocom", "BTC_USDT", "same correlation id for subscription and request");
         request.appendParam({
-            {"SIDE", "BUY"},
-            {"LIMIT_PRICE", "20000"},
-            {"QUANTITY", "0.001"},
+            {"ORDER_ID", "2111100095665909890"},
+            // {"LIMIT_PRICE", "20000"},
+            // {"QUANTITY", "0.000001"},
         });
         session->sendRequestByWebsocket(request);
       }
@@ -33,23 +45,23 @@ using ::ccapi::SessionOptions;
 using ::ccapi::Subscription;
 using ::ccapi::UtilSystem;
 int main(int argc, char** argv) {
-  if (UtilSystem::getEnvAsString("OKEX_API_KEY").empty()) {
-    std::cerr << "Please set environment variable OKEX_API_KEY" << std::endl;
-    return EXIT_FAILURE;
-  }
-  if (UtilSystem::getEnvAsString("OKEX_API_SECRET").empty()) {
-    std::cerr << "Please set environment variable OKEX_API_SECRET" << std::endl;
-    return EXIT_FAILURE;
-  }
-  if (UtilSystem::getEnvAsString("OKEX_API_PASSPHRASE").empty()) {
-    std::cerr << "Please set environment variable OKEX_API_PASSPHRASE" << std::endl;
-    return EXIT_FAILURE;
-  }
+  // if (UtilSystem::getEnvAsString("OKEX_API_KEY").empty()) {
+  //   std::cerr << "Please set environment variable OKEX_API_KEY" << std::endl;
+  //   return EXIT_FAILURE;
+  // }
+  // if (UtilSystem::getEnvAsString("OKEX_API_SECRET").empty()) {
+  //   std::cerr << "Please set environment variable OKEX_API_SECRET" << std::endl;
+  //   return EXIT_FAILURE;
+  // }
+  // if (UtilSystem::getEnvAsString("OKEX_API_PASSPHRASE").empty()) {
+  //   std::cerr << "Please set environment variable OKEX_API_PASSPHRASE" << std::endl;
+  //   return EXIT_FAILURE;
+  // }
   SessionOptions sessionOptions;
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Subscription subscription("okex", "BTC-USDT", "ORDER_UPDATE", "", "same correlation id for subscription and request");
+  Subscription subscription("cryptocom", "BTC_USDT", "ORDER_UPDATE,PRIVATE_TRADE", "", "same correlation id for subscription and request");
   session.subscribe(subscription);
   std::this_thread::sleep_for(std::chrono::seconds(10));
   session.stop();
--- example/src/market_data_simple_request/main.cpp
+++ example/src/market_data_simple_request/main.cpp
@@ -1,6 +1,18 @@
 #include "ccapi_cpp/ccapi_session.h"
 namespace ccapi {
-Logger* Logger::logger = nullptr;  // This line is needed.
+class MyLogger final : public Logger {
+ public:
+  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
+                  const std::string& lineNumber, const std::string& message) override {
+    std::lock_guard<std::mutex> lock(m);
+    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
+  }
+
+ private:
+  std::mutex m;
+};
+MyLogger myLogger;
+Logger* Logger::logger = &myLogger;
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
@@ -19,10 +31,10 @@ int main(int argc, char** argv) {
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Request request(Request::Operation::GET_RECENT_TRADES, "coinbase", "BTC-USD");
-  request.appendParam({
-      {"LIMIT", "1"},
-  });
+  Request request(Request::Operation::GET_INSTRUMENT, "cryptocom", argv[1]);
+  // request.appendParam({
+  //     {"LIMIT", "1"},
+  // });
   session.sendRequest(request);
   std::this_thread::sleep_for(std::chrono::seconds(10));
   session.stop();
